418167d0052c5cebde8ea881a6978a3f9cdfb924
tomiy <tom.chappaz1@gmail.com>
2019-05-21 17:30:11 +0200

    moving files n stuff and better drag algorithm

---
+++ js/loader.js
@@ -0,0 +1,118 @@
+(async () => {
+  var data = localStorage.getItem('json');
+  if (!data) data = await (await fetch('./js/default.json')).json();
+
+  data = JSON.parse(data);
+
+  for (let k in data.sections) {
+    let sectionData = data.sections[k];
+    let sectionContainer = document.createElement('div');
+    let section = document.createElement('div');
+    let label = document.createElement('div');
+    let items = document.createElement('div');
+
+
+    if (!localStorage.getItem(sectionData.bind)) {
+      localStorage.setItem(sectionData.bind, parseInt(localStorage.length) + 1);
+    }
+
+    sectionContainer.classList.add('drag-container');
+    sectionContainer.classList.add('section-container');
+    sectionContainer.id = sectionData.bind;
+    sectionContainer.style.order = localStorage.getItem(sectionData.bind);
+    sectionContainer.dataset.order = localStorage.getItem(sectionData.bind);
+
+    section.classList.add('section');
+    section.classList.add('drag-item');
+
+    label.innerHTML = sectionData.label;
+    label.classList.add('label');
+
+    if (sectionData.color) label.style.backgroundColor = `#${sectionData.color}`;
+
+    items.classList.add('items');
+
+    section.appendChild(label);
+
+    for (let i in sectionData.items) {
+      let itemData = sectionData.items[i];
+      let item = document.createElement('div');
+      let url = document.createElement('a');
+
+      item.classList.add('item');
+
+      url.href = itemData.url;
+      url.innerHTML = itemData.label;
+      url.target = '_blank';
+
+      item.appendChild(url);
+      items.appendChild(item);
+    }
+
+    section.appendChild(items);
+    sectionContainer.appendChild(section);
+    container.appendChild(sectionContainer);
+  }
+
+  let items = [...document.querySelectorAll('.drag-item')];
+  let cur = null;
+  let initial = {
+    x: 0,
+    y: 0
+  }
+
+  document.addEventListener('mouseup', () => {
+    if (cur) {
+
+      let curRect = cur.getBoundingClientRect();
+      let target =
+        fElementsFromPoint(curRect.left + curRect.width / 2, curRect.top + curRect.height / 2)
+          .filter(el => el.matches('.drag-container')).pop();
+      if (target) {
+
+        targetId = target.id;
+
+        target.id = cur.parentNode.id;
+        cur.parentNode.id = targetId;
+
+        localStorage.setItem(cur.parentNode.id, cur.parentNode.dataset.order);
+        localStorage.setItem(target.id, target.dataset.order);
+
+        [...target.children].forEach(el => {
+          cur.parentNode.appendChild(el);
+        });
+        target.appendChild(cur);
+      }
+
+      cur.style.transform = null;
+      cur = null;
+    }
+  });
+
+  document.addEventListener('mousemove', e => {
+    if (cur) {
+      offsetX = e.clientX - initial.x;
+      offsetY = e.clientY - initial.y;
+      cur.style.transform = "translate3d(" + offsetX + "px, " + offsetY + "px, 0)";
+    }
+  });
+
+  items.forEach(item => {
+    item.querySelector('.label').addEventListener('mousedown', e => {
+      initial.x = e.clientX;
+      initial.y = e.clientY;
+      cur = item;
+      target = item.parentNode;
+    });
+  });
+})();
+
+const fElementsFromPoint = (x, y, l) => {
+  let element = document.elementFromPoint(x, y);
+  if (element !== l && element !== document.documentElement) {
+    element.style['pointerEvents'] = 'none';
+    let result = [element].concat(fElementsFromPoint(x, y, element));
+    element.style['pointerEvents'] = 'auto';
+    return result;
+  } else return [];
+};
---
+++ js/options.js
@@ -0,0 +1,24 @@
+(async () => {
+  var defaultJson = JSON.stringify(await (await fetch('./js/default.json')).json(), null, 2);
+
+  if (!localStorage.getItem('json')) {
+    localStorage.setItem('json', defaultJson);
+    jsoneditor.value = defaultJson;
+  } else jsoneditor.value = localStorage.getItem('json');
+
+  jsoneditor.addEventListener('change', function () {
+    try {
+      JSON.parse(jsoneditor.value);
+      localStorage.setItem('json', jsoneditor.value);
+      jsoneditor.style.backgroundColor = 'transparent';
+    } catch (e) {
+      jsoneditor.style.backgroundColor = 'red';
+    }
+  });
+
+  storagereset.addEventListener('click', function () {
+    var backupJson = localStorage.getItem('json');
+    localStorage.clear();
+    localStorage.setItem('json', backupJson);
+  });
+})();
