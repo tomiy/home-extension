8300945472a822cfe06d5962143f2e8f6c059e05
tomiy <tom.chappaz1@gmail.com>
2019-05-23 17:24:31 +0200

    options previewer and new section button

--- js/options.js
+++ js/options.js
@@ -1,5 +1,5 @@
 (async () => {
-  var defaultJson = JSON.stringify(await (await fetch('./js/default.json')).json(), null, 2);
+  let defaultJson = JSON.stringify(await (await fetch('./js/default.json')).json(), null, 2);
 
   if (!localStorage.getItem('json')) {
     localStorage.setItem('json', defaultJson);
@@ -11,14 +11,33 @@
       JSON.parse(jsoneditor.value);
       localStorage.setItem('json', jsoneditor.value);
       jsoneditor.style.backgroundColor = 'transparent';
+
+      previewer.contentWindow.location.reload();
     } catch (e) {
-      jsoneditor.style.backgroundColor = 'red';
+      jsoneditor.style.backgroundColor = '#dbc';
     }
   });
 
   storagereset.addEventListener('click', function () {
-    var backupJson = localStorage.getItem('json');
+    let backupJson = localStorage.getItem('json');
     localStorage.clear();
     localStorage.setItem('json', backupJson);
+    previewer.contentWindow.location.reload();
+  });
+
+  newsection.addEventListener('click', function () {
+    let json = JSON.parse(localStorage.getItem('json'));
+    json.sections.push({
+      bind: 'changeit',
+      label: 'Change it',
+      items: [{
+        label: 'Example item',
+        url: 'example.com'
+      }]
+    });
+    jsoneditor.value = JSON.stringify(json, null, 2);
+    localStorage.setItem('json', JSON.stringify(json));
+
+    previewer.contentWindow.location.reload();
   });
 })();
