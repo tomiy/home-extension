e07b8759797714acc663b95cce1b64d7033f3e6e
tomiy <tom.chappaz1@gmail.com>
2019-09-20 10:41:22 +0200

    add drag & drop

--- js/loaderv2.js
+++ js/loaderv2.js
@@ -1,11 +1,11 @@
 import $Application from './class/Application.js';
 import $Event from './class/Event.js';
 
-const env = document.scripts[0].getAttribute('env');
-
-const app = new $Application(env);
+const app = new $Application();
 app.getData();
 
 const documentEvents = new $Event(document);
 documentEvents.register('DOMContentLoaded', app.loadDOM.bind(app));
-documentEvents.delegate('click', (e) => alert('test'), '.drag-container', '.section-container');
+documentEvents.delegate('mousedown', app.pickup.bind(app), '.label');
+documentEvents.register('mousemove', app.move.bind(app));
+documentEvents.register('mouseup', app.drop.bind(app));
