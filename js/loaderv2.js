79ecb107a3c73e401dbf627e0637433f7e0944a0
tomiy <tom.chappaz1@gmail.com>
2019-09-27 14:45:14 +0200

    basic popup functionality

--- js/loaderv2.js
+++ js/loaderv2.js
@@ -4,20 +4,34 @@ import $Popup from './class/Popup.js';
 
 const app = new $Application();
 const documentEvents = new $Event(document);
-const popup = new $Popup();
-
+const popupContainer = typeof(overlay) === 'undefined' ? document.createElement('span') : overlay;
+const popup = new $Popup(popupContainer);
 //get objects from localStorage
 app.getData();
 
 //bind main events to app
 documentEvents.setBind(app);
 
-//load DOM and move events
-documentEvents.register('DOMContentLoaded', app.loadDOM);
-documentEvents.delegate('mousedown', app.pickup, '.label');
-documentEvents.register('mousemove', app.move);
-documentEvents.register('mouseup', app.drop);
+documentEvents.register('DOMContentLoaded', () => {
+    //load DOM and move events
+    app.loadDOM();
 
-documentEvents.register('dragstart', e => e.preventDefault());
+    //delegated because indefinite amount of elements
+    documentEvents.delegate('mousedown', app.pickup, '.label');
+    documentEvents.register('mousemove', app.move);
+    documentEvents.register('mouseup', app.drop);
 
-//load Popup and option events
+    documentEvents.register('dragstart', e => e.preventDefault());
+
+    //load Popup and option events
+    const popupCloseEvent = new $Event(document.querySelector('#popup-close'));
+    const addSectionEvent = new $Event(document.querySelector('#add-section'));
+
+    popupCloseEvent.setBind(popup);
+    addSectionEvent.setBind(popup);
+
+    documentEvents.register('keydown', e => e.keyCode == 27 && popup.close());
+    popupCloseEvent.register('click', popup.close);
+
+    addSectionEvent.register('click', popup.showCreateSection);
+});
