8a73ba3ae01772e98f44ae2ae4172d8581bf02f9
tomiy <tom.chappaz1@gmail.com>
2019-09-27 10:42:27 +0200

    init popup and overlay

--- js/loaderv2.js
+++ js/loaderv2.js
@@ -1,15 +1,23 @@
 import $Application from './class/Application.js';
 import $Event from './class/Event.js';
+import $Popup from './class/Popup.js';
 
 const app = new $Application();
+const documentEvents = new $Event(document);
+const popup = new $Popup();
+
+//get objects from localStorage
 app.getData();
 
-const documentEvents = new $Event(document);
+//bind main events to app
 documentEvents.setBind(app);
 
+//load DOM and move events
 documentEvents.register('DOMContentLoaded', app.loadDOM);
 documentEvents.delegate('mousedown', app.pickup, '.label');
 documentEvents.register('mousemove', app.move);
 documentEvents.register('mouseup', app.drop);
 
 documentEvents.register('dragstart', e => e.preventDefault());
+
+//load Popup and option events
