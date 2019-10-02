3c5ebfc53989825765a1017b457a7d724f60dbe3
tomiy <tom.chappaz1@gmail.com>
2019-10-02 16:58:33 +0200

    Delete item

---
+++ js/class/form/DeleteItemForm.js
@@ -0,0 +1,43 @@
+import $Form from "./Form.js";
+import $SectionParser from "../dom/SectionParser.js";
+import $Utils from "../Utils.js";
+
+export default class $DeleteItemForm extends $Form {
+    generate(item) {
+        let formObj = {
+            header: 'Delete Item?',
+            fields: [{
+                    type: 'hidden',
+                    name: 'item-index',
+                    value: $Utils.getNodeIndex(item)
+                },
+                {
+                    type: 'hidden',
+                    name: 'section-id',
+                    value: item.closest('.section-container').id
+                }
+            ]
+        };
+
+        this.objectToForm(formObj);
+
+        this.reset();
+
+        return this.form;
+    }
+
+    submit(app) {
+        let formData = this.formToObject();
+
+        app.data.updateObject((data) => {
+            let sectionId = $SectionParser.getSectionId(data, formData['section-id']);
+
+            document.querySelector(
+                `#${formData['section-id']} .item:nth-child(${parseInt(formData['item-index']) + 1})`
+            ).remove();
+            data.sections[sectionId].items.splice(formData['item-index'], 1);
+
+            return data;
+        });
+    }
+}
