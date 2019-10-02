5bef18d48ff22ccfd6e0433ac13958ffb3a63523
tomiy <tom.chappaz1@gmail.com>
2019-10-02 11:29:11 +0200

    Delete section form and menu entry

--- js/class/menu/SectionContextMenu.js
+++ js/class/menu/SectionContextMenu.js
@@ -4,8 +4,13 @@ export default class $SectionContextMenu extends $ContextMenu {
     constructor(popup) {
         super();
         this.items = [{
-            label: 'Edit section',
-            callback: (sectionlb) => popup.show('editSection', sectionlb[0].closest('.section-container'))
-        }];
+                label: 'Edit section',
+                callback: (sectionlb) => popup.show('editSection', sectionlb[0].closest('.section-container'))
+            },
+            {
+                label: 'Delete section',
+                callback: (sectionlb) => popup.show('deleteSection', sectionlb[0].closest('.section-container'))
+            }
+        ];
     }
 }
