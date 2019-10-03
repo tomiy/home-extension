40cc425189031cf191aa2afd75344bdea4bb0f4a
tomiy <tom.chappaz1@gmail.com>
2019-10-03 14:01:40 +0200

    Edit item form + form value fix

---
+++ js/class/form/EditItemForm.js
@@ -0,0 +1,73 @@
+import $Form from "./Form.js";
+import $Utils from "../Utils.js";
+import $SectionParser from "../dom/SectionParser.js";
+
+export default class $EditItemForm extends $Form {
+    generate(item) {
+        let formObj = {
+            header: 'Edit Section',
+            fields: [{
+                    type: 'text',
+                    name: 'item-name',
+                    label: 'Name',
+                    value: item.innerText
+                },
+                {
+                    type: 'text',
+                    name: 'item-link',
+                    label: 'Link',
+                    value: item.querySelector('a').href
+                },
+                {
+                    type: 'hidden',
+                    name: 'item-index',
+                    value: $Utils.getNodeIndex(item)
+                },
+                {
+                    type: 'hidden',
+                    name: 'section-id',
+                    value: item.closest('.section-container').id
+                }
+            ]
+        };
+
+        this.objectToForm(formObj);
+
+        this.reset();
+
+        return this.form;
+    }
+
+    submit(app) {
+        let formData = this.formToObject();
+
+        if (!formData['item-name'].trim().length) {
+            this.errors.content('Cannot have empty name');
+            return false;
+        }
+
+        if (!formData['item-link'].trim().length) {
+            this.errors.content('Cannot have empty link');
+            return false;
+        }
+
+        if (!$Utils.isURL(formData['item-link'])) {
+            this.errors.content('Link is not a valid URL');
+            return false;
+        }
+
+        app.data.updateObject((data) => {
+            let sectionId = $SectionParser.getSectionId(data, formData['section-id']);
+            let itemDOM = document.querySelector(
+                `#${formData['section-id']} .item:nth-child(${parseInt(formData['item-index']) + 1})`
+            );
+
+            data.sections[sectionId].items[formData['item-index']].label = formData['item-name'];
+            data.sections[sectionId].items[formData['item-index']].url = formData['item-link'];
+
+            $SectionParser.updateItemDOM(data.sections[sectionId], formData['item-index']);
+
+            return data;
+        });
+    }
+}
